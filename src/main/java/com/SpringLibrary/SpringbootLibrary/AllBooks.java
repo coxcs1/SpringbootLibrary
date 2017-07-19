25a30e0348464d0e1b9984f39da09ffc645572a2
ricky-clevinger <ricky.clevinger@cgi.com>
2017-07-19 10:40:18 -0400

    Comments
    Comments added to allBooks.java.
    Some name changes to methods.

--- src/main/java/com/SpringLibrary/SpringbootLibrary/AllBooks.java
+++ src/main/java/com/SpringLibrary/SpringbootLibrary/AllBooks.java
@@ -20,6 +20,8 @@ import java.util.Set;
 
 /**
  * Created by ricky.clevinger on 7/12/2017.
+ *
+ * last modified by ricky.clevinger on 7/19/17
  */
 @SpringView(name = AllBooks.VIEW_NAME)
 public class AllBooks extends VerticalLayout implements View {
@@ -32,8 +34,10 @@ public class AllBooks extends VerticalLayout implements View {
 
     @PostConstruct
     /**
-     * Retrieves the data and constructs the view.
-     * Creates and populates the grid.
+     * Initializes the view..
+     * Calls createFilter to create search filter for the grid.
+     * Calls createGrid to create and populate the grid.
+     * Calls createDeleteButton to create delete button and add functionality to the button.
      *
      * last modified by ricky.clevinger 7/19/17
      */
@@ -42,8 +46,14 @@ public class AllBooks extends VerticalLayout implements View {
         createFilter();
         createGrid();
         createDeleteButton();
-    }
+    }//end init
 
+    /**
+     * Method add delete button to view.
+     * On button click the selected item in the grid will be removed from grid and backing micro-service.
+     *
+     * last modified by ricky.clevinger 7/19/17
+     */
     public void createDeleteButton() {
 
         // Delete button to remove selected item from the grid as well as the micro-service.
@@ -54,11 +64,12 @@ public class AllBooks extends VerticalLayout implements View {
         });
         // Add delete button to the view.
         addComponent(delete);
-    }
+    }//end createDeleteButton
 
     /**
-     * Function shall retrive data.
-     * User shall type in part of a book title and the grid will changed accordingly.
+     * Function shall retrieve data from microservice.
+     * Creates and populates the grid.
+     * Adds listener to record the user selected item.
      *
      * last modified by ricky.clevinger 7/19/17
      */
@@ -70,9 +81,6 @@ public class AllBooks extends VerticalLayout implements View {
         // Create a grid and adds listener to record selected item.
         grid = new Grid<>();
         grid.addSelectionListener(event -> {
-            Set<Book> selected = event.getAllSelectedItems();
-            //Notification.show(selected.size() + " items selected");
-            Notification.show(event.getFirstSelectedItem().get().getBookId() + "");
             this.id = event.getFirstSelectedItem().get().getBookId() + "";
         });
 
@@ -99,23 +107,24 @@ public class AllBooks extends VerticalLayout implements View {
         titleFilter = new TextField();
         titleFilter.setWidth(100, Unit.PERCENTAGE);
         titleFilter.setPlaceholder("Book Title...");
-        titleFilter.addValueChangeListener(this::onNameFilterTextChange);
+        titleFilter.addValueChangeListener(this::titleFilterGridChange);
         addComponent(titleFilter);
-    }
+    }//end createFilter
 
     /**
-     * Helper function for the FilteredGridLayout.
-     * Chnages the grid and compares the titles.
+     * Helper function for the createFilter.
+     * Changes the grid and compares the titles.
      * @param event
      * last modified by ricky.clevinger 7/19/17
      */
-    private void onNameFilterTextChange(HasValue.ValueChangeEvent<String> event) {
+    private void titleFilterGridChange(HasValue.ValueChangeEvent<String> event) {
         ListDataProvider<Book> dataProvider = (ListDataProvider<Book>) grid.getDataProvider();
         dataProvider.setFilter(Book::getTitle, s -> caseInsensitiveContains(s, event.getValue()));
-    }
+    }//end titleFilterGridChange
 
     /**
-     *Returns a boolean telling if the lowercase form of text input into the filter is contain by any of the lowercase versions of the book titles.
+     *Returns a boolean telling if the lowercase form of text input into the filter is contain
+     * by any of the lowercase versions of the book titles.
      * @param where the books titles its comparing to
      * @param what  the filter wood being compared to the book titles
      * @return Boolean telling if the lower case value of the filter input and the book titles match
@@ -124,7 +133,7 @@ public class AllBooks extends VerticalLayout implements View {
      */
     private Boolean caseInsensitiveContains(String where, String what) {
         return where.toLowerCase().contains(what.toLowerCase());
-    }
+    }//end caseInsensitiveContains
 
 
     @Override
