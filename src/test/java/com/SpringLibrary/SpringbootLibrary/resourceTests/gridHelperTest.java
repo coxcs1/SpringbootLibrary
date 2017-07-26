deb448b53c0d6448df293024d6042c16d641fd2a
ricky-clevinger <ricky.clevinger@cgi.com>
2017-07-26 16:00:50 -0400

    More Unit tests

--- src/test/java/com/SpringLibrary/SpringbootLibrary/resourceTests/gridHelperTest.java
+++ src/test/java/com/SpringLibrary/SpringbootLibrary/resourceTests/gridHelperTest.java
@@ -5,42 +5,47 @@ import org.junit.Test;
 
 import java.sql.Date;
 
+import static Resource.gridHelper.caseInsensitiveContains;
 import static Resource.gridHelper.overdue;
 import static Resource.gridHelper.stringClean;
 
 public class gridHelperTest {
+    Boolean resultBoolean;
+    Boolean expectedBoolean;
+    String expectedString;
+    String resultString;
 
     @Test
     public void stringCleanBadInputTest() throws Exception {
 
         //What the expected result is
-        String expected = "";
+        expectedString = "";
 
         //Sets the result using getter
-        String result = stringClean("3");
+        resultString = stringClean("3");
         //Compares expected result with the actual result.
-        Assert.assertEquals(expected, result);
+        Assert.assertEquals(expectedString, resultString);
 
-        result = stringClean("!23214");
+        resultString = stringClean("!23214");
 
-        Assert.assertEquals(expected, result);
+        Assert.assertEquals(expectedString, resultString);
     }
 
     @Test
     public void stringCleanGoodInputTest() throws Exception {
 
         //What the expected result is
-        String expected = "Cat";
+        expectedString = "Cat";
 
         //Sets the result using getter
-        String result = stringClean("Cat");
+        resultString = stringClean("Cat");
         //Compares expected result with the actual result.
-        Assert.assertEquals(expected, result);
+        Assert.assertEquals(expectedString, resultString);
 
-        expected = "Turtle";
-        result = stringClean("Turtle");
+        expectedString = "Turtle";
+        resultString = stringClean("Turtle");
 
-        Assert.assertEquals(expected, result);
+        Assert.assertEquals(expectedString, resultString);
     }
 
     @Test
@@ -50,12 +55,30 @@ public class gridHelperTest {
         Date date2 = new Date(1);
 
         //What the expected result is
-        String expected = "Overdue";
+        expectedString = "Overdue";
 
         //Sets the result using getter
         String result = overdue(date1,date2);
         //Compares expected result with the actual result.
-        Assert.assertEquals(expected, result);
+        Assert.assertEquals(expectedString, result);
+
+    }
+
+    @Test
+    public void caseInsensitiveContainsTest() throws Exception {
+
+        //What the expected result is
+        expectedBoolean = true;
+
+        //Sets the result using getter
+        resultBoolean = caseInsensitiveContains("ROAR","ro");
+        //Compares expected result with the actual result.
+        Assert.assertEquals(expectedBoolean, resultBoolean);
+
+        expectedBoolean = false;
+        resultBoolean = caseInsensitiveContains("ROAR","rooooo");
+
+        Assert.assertEquals(expectedBoolean, resultBoolean);
 
     }
 }
