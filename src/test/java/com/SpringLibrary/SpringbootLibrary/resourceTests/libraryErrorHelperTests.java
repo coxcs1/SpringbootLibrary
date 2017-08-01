9e377b45bf4ac870c28cd7c245a7e263a911a671
ricky-clevinger <ricky.clevinger@cgi.com>
2017-08-01 10:59:07 -0400

    Error Fixing

--- src/test/java/com/SpringLibrary/SpringbootLibrary/resourceTests/libraryErrorHelperTests.java
+++ src/test/java/com/SpringLibrary/SpringbootLibrary/resourceTests/libraryErrorHelperTests.java
@@ -4,23 +4,18 @@ import Resource.LibraryErrorHelper;
 import org.junit.Assert;
 import org.junit.Test;
 
-
-import static Resource.gridHelper.*;
-
 public class libraryErrorHelperTests {
-    private String expectedString;
-    private String resultString;
-    LibraryErrorHelper libraryErrorHelper = new LibraryErrorHelper();
-    Exception e = new Exception();
+    private LibraryErrorHelper libraryErrorHelper = new LibraryErrorHelper();
+    private Exception e = new Exception();
 
     @Test
     public void genericErrorTest() throws Exception {
 
         //What the expected result is
-        expectedString = "The string was empty";
+        String expectedString = "The string was empty";
 
         //Sets the result using getter
-        resultString = libraryErrorHelper.genericError(e);
+        String resultString = libraryErrorHelper.genericError(e);
         //Compares expected result with the actual result.
         Assert.assertEquals(expectedString, resultString);
 
