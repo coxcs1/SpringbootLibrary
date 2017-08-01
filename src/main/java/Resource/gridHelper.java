e83197ed71dc22697e52515431730287cf55aaf4
ricky-clevinger <ricky.clevinger@cgi.com>
2017-08-01 14:43:05 -0400

    YML Commented and Overdue fixed
    YML variable commented out.
    
    Fixed overdue to work the same as the GO team

--- src/main/java/Resource/gridHelper.java
+++ src/main/java/Resource/gridHelper.java
@@ -9,6 +9,7 @@ import com.vaadin.navigator.ViewChangeListener;
 import com.vaadin.ui.Grid;
 import com.vaadin.ui.VerticalLayout;
 import java.sql.Date;
+import java.util.Calendar;
 
 public class gridHelper extends VerticalLayout implements View
 {
@@ -99,9 +100,14 @@ public class gridHelper extends VerticalLayout implements View
      */
     public static String overdue(Date date1, Date date2)
     {
-        if (date1.compareTo(date2) > 0)
+        if ((date1.getTime() + (1000*60*60*24*7) ) > (date2.getTime()))
         {
-            return date1.toString();
+            Calendar c = Calendar.getInstance();
+            c.setTime(date1);
+            c.add(Calendar.DATE, 7);
+            c.getTime();
+
+            return new Date(c.getTimeInMillis()) + "";
         }
         else
         {
