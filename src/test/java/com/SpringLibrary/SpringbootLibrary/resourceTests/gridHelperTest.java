6dfb2e812e864af10d20a10bdf2ff208ca589370
ricky-clevinger <ricky.clevinger@cgi.com>
2017-08-04 10:20:18 -0400

    Unit Test Changes

--- src/test/java/com/SpringLibrary/SpringbootLibrary/resourceTests/gridHelperTest.java
+++ src/test/java/com/SpringLibrary/SpringbootLibrary/resourceTests/gridHelperTest.java
@@ -70,6 +70,22 @@ public class gridHelperTest
         resultBoolean = caseInsensitiveContains("ROAR","rooooo");
 
         Assert.assertEquals(false, resultBoolean);
-
     }
+
+
+    @Test
+    public void overdueTest() throws Exception
+    {
+
+        //Sets the result using getter
+        Date date1 = new Date(2001-01-19);
+        Date date2 = new Date(2000-01-19);
+        //Compares expected result with the actual result.
+
+        resultString = overdue(date1,date2);
+
+        //System.out.println(resultString);
+        //Assert.assertEquals(false, resultString);
+    }
+
 }
